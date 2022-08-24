ef62bb6825b3e8fffc591fc95fc04183ed93f71a
MMLaw95 <103419849+MMLaw95@users.noreply.github.com>
2022-08-24 02:48:39 -0500

    Comment out axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,5 @@
 import React from "react";
-import axios from "axios";
+// import axios from "axios";
 // import ReactDOM from "react-dom";
 // import Cities from "./Cities";
 // import Icon from "./Icon";
