d2fd9d83077081c8ff0da28ac4f7a9e4d5581e35
MMLaw95 <103419849+MMLaw95@users.noreply.github.com>
2022-08-29 06:27:17 -0500

    Clean up react app files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Weather from "./Weather";
+import Footer from "./Footer";
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Weather />
+      <Footer />
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import Weather from "./Weather";
-import Footer from "./Footer";
-import "./Styles.css";
-import "./Footer.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Weather />
-      <Footer />
-    </div>
-  );
-}
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
