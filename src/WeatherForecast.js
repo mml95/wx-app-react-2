97d43b9fe4edf9b8d45be3be0cd731ca3d93bd8a
MMLaw95 <103419849+MMLaw95@users.noreply.github.com>
2022-09-06 12:11:51 -0500

    Update api

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,44 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
 import "./Weather.css";
 import axios from "axios";
 
 export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
-    console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
 
-  const apiKey = "a4291214a1e333b12b6de7b256df44ea";
-  let longitude = props.coordinates.lon;
-  let latitude = props.coordinates.lat;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
-
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <div className="row group-days px-md-5">
-      <div className="col-md g-3 border-0">
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item border-0" id="day-week">
-            <span className="weather-forecast-day">Sun</span>
-          </li>
-          <li className="list-group-item border-0" id="emoji-week">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt=""
-              className="current-emoji"
-              width="30"
-            />
-          </li>
-          <li className="list-group-item" id="high-low-week">
-            <span className="highs" id="high-weekday">
-              {80}
-            </span>
-            <span className="lows" id="low-weekday">
-              {95}
-            </span>
-          </li>
-        </ul>
+  if (loaded) {
+    return (
+      <div className="row group-days px-md-5">
+        <div className="col-md g-3 border-0">
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item border-0" id="day-week">
+              <span className="weather-forecast-day">{forecast[0].dt}</span>
+            </li>
+            <li className="list-group-item border-0" id="emoji-week">
+              {/* <img
+                src={props.data.iconUrl}
+                alt={props.data.description}
+                width="30"
+                className="d-block current-emoji"
+              /> */}
+            </li>
+            <li className="list-group-item" id="high-low-week">
+              <span className="highs" id="high-weekday">
+                {forecast[0].temp.max}°
+              </span>
+              <span className="lows" id="low-weekday">
+                {forecast[0].temp.min}°
+              </span>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "c34fe5357ee430b2fa097f83b1285021";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  }
 }
